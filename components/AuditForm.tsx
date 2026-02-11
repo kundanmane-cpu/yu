d3d86a503a392e8b2d16cbf50f4b37b37ee0c9da
kundanmane-cpu <kundanmane@thrivexventures.com>
2026-02-12 00:47:29 +0530

    Update GOOGLE_SCRIPT_URL to new endpoint

--- components/AuditForm.tsx
+++ components/AuditForm.tsx
@@ -4,7 +4,7 @@ import { motion, AnimatePresence } from 'framer-motion';
 import { Send, CheckCircle2, Sparkles, Instagram, Target, User, Mail, Phone } from 'lucide-react';
 import { AuditFormData } from '../types';
 
-const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbz0iTg0GOAbLPy_1YPEYwdm4zlXE5ygI7tDWRr81W1wmhbxjzXT5h5kN5rK5RX-qFG0/exec';
+const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwoTfNkeE4133UUANBObga7CmDklRVT5MMb548l93nObiYYWD8NKzs9QrpvQ3N9oXuo/exec';
 
 const AuditForm: React.FC = () => {
   const [formData, setFormData] = useState<AuditFormData>({
