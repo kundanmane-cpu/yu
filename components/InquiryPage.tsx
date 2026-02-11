f3bdc2f9b9f75c1ddf3ad13e13596820a6ad0612
kundanmane-cpu <kundanmane@thrivexventures.com>
2026-02-12 00:48:46 +0530

    Update GOOGLE_SCRIPT_URL to new endpoint

--- components/InquiryPage.tsx
+++ components/InquiryPage.tsx
@@ -15,7 +15,7 @@ import {
 } from 'lucide-react';
 import { PRICING_PLANS } from '../constants';
 
-const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbz0iTg0GOAbLPy_1YPEYwdm4zlXE5ygI7tDWRr81W1wmhbxjzXT5h5kN5rK5RX-qFG0/exec';
+const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwoTfNkeE4133UUANBObga7CmDklRVT5MMb548l93nObiYYWD8NKzs9QrpvQ3N9oXuo/exec';
 
 interface InquiryPageProps {
   onBack: () => void;
